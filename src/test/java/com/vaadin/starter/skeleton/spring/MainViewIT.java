35c87801e1b82552f29578280efb0308362159bc
Stefan Sedelmaier <stefan@sedelmaier.at>
2019-02-07 14:05:12 +0100

    add Vaadin Selenium Integration-Test

--- src/test/java/com/vaadin/starter/skeleton/spring/MainViewIT.java
+++ src/test/java/com/vaadin/starter/skeleton/spring/MainViewIT.java
@@ -1,7 +1,5 @@
 package com.vaadin.starter.skeleton.spring;
 
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.testbench.TestBenchTestCase;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
@@ -9,13 +7,17 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MainViewIT extends TestBenchTestCase {
+public class MainViewIT {
 
     private static int SERVERPORT = Integer.valueOf(System.getProperty("test.server.port"));
+
+    private WebDriver webDriver;
 
     @BeforeClass
     public static void setupClass() {
@@ -24,30 +26,33 @@ public class MainViewIT extends TestBenchTestCase {
 
     @Before
     public void setUp() throws Exception {
-        setDriver(new FirefoxDriver());
+        // Create a new browser instance
+        webDriver = new FirefoxDriver();
     }
 
     @After
     public void teardown() {
-        getDriver().quit();
+        if (webDriver != null) {
+            webDriver.quit();
+        }
     }
 
     @Ignore
     @Test
     public void testBowerComponentsAreLoadable() {
         // /frontend/bower_components/webcomponentsjs/webcomponents-loader.js must be loadable
-        getDriver().get(String.format("http://localhost:%s/frontend/bower_components/webcomponentsjs/webcomponents-loader.js", SERVERPORT));
-        System.out.println(getDriver().getPageSource());
+        webDriver.get(String.format("http://localhost:%s/frontend/bower_components/webcomponentsjs/webcomponents-loader.js", SERVERPORT));
+        System.out.println(webDriver.getPageSource());
     }
 
     @Test
     public void clickMeMustShowNotification() {
-        getDriver().get(String.format("http://localhost:%s", SERVERPORT));
+        WebDriverWait wait = new WebDriverWait(webDriver, 20);
+        webDriver.get(String.format("http://localhost:%s", SERVERPORT));
 
-        ButtonElement clickmeButton = $(ButtonElement.class).first();
+        WebElement clickmeButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("vaadin-button")));
         clickmeButton.click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), 10);
         //<div part="content" class="style-scope vaadin-notification-card">Button was clicked at 18:30:47.795</div>
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("vaadin-notification-card")));
     }
