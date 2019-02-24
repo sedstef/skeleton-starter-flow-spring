a3773c5d84d5f8c7caa76a2ac536bb84e656a85c
Stefan Sedelmaier <stefan@sedelmaier.at>
2019-02-24 11:36:52 +0100

    add Grid to demonstrate setFullSize problems

--- src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
+++ src/main/java/com/vaadin/starter/skeleton/spring/MainView.java
@@ -1,12 +1,15 @@
 package com.vaadin.starter.skeleton.spring;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import java.util.ArrayList;
+import java.util.List;
 
 @Route
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
@@ -16,6 +19,20 @@ public class MainView extends VerticalLayout {
         Button button = new Button("Click me",
                 e -> Notification.show(bean.getMessage()));
         add(button);
+        
+        Grid<String> grid = new Grid<>();
+        grid.addColumn(source -> source);
+        grid.setItems(createItems(bean));
+        grid.setSizeFull();
+        add(grid);
+    }
+
+    private List<String> createItems(MessageBean bean) {
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            items.add(bean.getMessage());
+        }
+        return items;
     }
 
 }
